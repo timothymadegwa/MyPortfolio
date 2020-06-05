c299cf1437667676b1fa55eb06b9b04d5a065964
Timothy Madegwa <timothymadegwa@gmail.com>
2020-06-05 11:28:18 +0300

    intial setup of django app

---
+++ portfolio/app/urls.py
@@ -0,0 +1,11 @@
+from django.urls import path
+
+from . import views
+
+urlpatterns = [
+    path('', views.index, name='index'),
+    path('portfolio', views.portfolio, name='portfolio'),
+    path('project', views.project, name='project'),
+    path('blog', views.blog, name='blog'),
+    path('cv', views.cv, name='cv'),
+]
---
+++ portfolio/app/views.py
@@ -0,0 +1,18 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+
+# Create your views here.
+def index(request):
+    return render(request, 'app/index.html')
+
+def portfolio(request):
+    return render(request, 'app/portfolio.html')
+
+def project(request):
+    return render(request, 'app/project.html')
+
+def blog(request):
+    return render(request, 'app/blog.html')
+
+def cv(request):
+    return render(request, 'app/cv.html')
