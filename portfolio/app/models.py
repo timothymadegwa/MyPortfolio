4c5cc36ac7afb44a090b2a06307ce19131d61f0a
Timothy Madegwa <timothymadegwa@gmail.com>
2020-06-06 15:21:51 +0300

    created the contact model and made migrations to the DB

--- portfolio/app/models.py
+++ portfolio/app/models.py
@@ -1,3 +1,16 @@
 from django.db import models
-
+from django.utils import timezone
 # Create your models here.
+
+class Contact(models.Model):
+
+    first_name = models.CharField(max_length=100)
+    last_name = models.CharField(max_length=100)
+    email = models.CharField(max_length=100)
+    phone = models.CharField(max_length=100)
+    message = models.TextField(blank=True)
+    contact_date = models.DateTimeField(default=timezone.now)
+
+
+    def __str__(self):
+        return self.first_name + ' ' + self.last_name
--- portfolio/app/views.py
+++ portfolio/app/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from .models import Contact
 
 # Create your views here.
 def index(request):
@@ -18,4 +19,16 @@ def cv(request):
     return render(request, 'app/cv.html')
 
 def contact(request):
+    if request.method == 'POST':
+        f_name = request.POST['first_name']
+        l_name = request.POST['last_name']
+        email = request.POST['email']
+        phone = request.POST['phone']
+        message = request.POST['message']
+
+        contact =Contact(first_name=f_name, last_name=l_name, email=email,phone=phone,message=message)
+        contact.save()
+
+        return render(request, 'app/contact.html', )
+
     return render(request, 'app/contact.html')
