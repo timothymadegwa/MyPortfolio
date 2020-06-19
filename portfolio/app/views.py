ae2392a1cab2909565d1b840543da69ad4ea2864
Timothy Madegwa <timothymadegwa@gmail.com>
2020-06-19 14:38:06 +0300

    added the mailing function

--- portfolio/app/views.py
+++ portfolio/app/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 from .models import Contact
+from smtplib import SMTP
 
 import datetime
 
@@ -30,6 +31,12 @@ def contact(request):
 
         contact =Contact(first_name=f_name, last_name=l_name, email=email,phone=phone,message=message, contact_date=datetime.datetime.now())
         contact.save()
+        message = 'You have recieved a message from '+f_name+' '+l_name+' | '+email+' | '+phone+' \n'+message
+        server = SMTP('smtp.gmail.com' , 587)
+        server.starttls()
+        server.login('aiaqila20@gmail.com', 'aqila123?')
+        server.sendmail('aiaqila20@gmail.com', 'timothy@aqila.co.ke', message)
+        server.quit()
         reply_body = 'Thank you {},  Your message has been sent successfully'.format(f_name)
 
         reply = {'message': reply_body }
